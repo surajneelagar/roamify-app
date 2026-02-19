2e6e0129bed851950fc6c9169278075a775c0337
Suraj S Neelagar <71820711+surajneelagar@users.noreply.github.com>
2026-02-19 23:20:44 +0530

    refactor: Simplify bootstrapApplication configuration in main.ts and app.config.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { provideZoneChangeDetection } from '@angular/core';
+import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)
+  ]
+}).catch(err => console.error(err));
