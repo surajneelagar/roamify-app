d715e708f6b7fe27def099a3368180aa9a85ef1e
Suraj S Neelagar <71820711+surajneelagar@users.noreply.github.com>
2026-02-20 00:30:43 +0530

    refactor: Consolidate application configuration in app.config.ts and main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { provideZoneChangeDetection } from '@angular/core';
-import { routes } from './app/app.routes';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
